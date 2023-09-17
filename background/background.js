fa5e3478521577b29caacfa1742dad8cfad9d8cf
daiki_win <kubotadaiki0654@gmail.com>
2023-09-17 15:45:16 +0900

    feat: Notionへの登録時のエラーを通知する機能を追加(#3)

--- background/background.js
+++ background/background.js
@@ -2,21 +2,32 @@ chrome.runtime.onMessage.addListener(function (info, sender, sendResponse) {
   chrome.storage.local.get(
     ["notionToken", "databaseId"],
     async function (value) {
-      const isNotRegistered = await checkRegistered(value, info);
-
-      if (isNotRegistered) {
-        registerWithNotion(value, info);
+      // 登録しようとした動画がすでに登録済みかチェック
+      const response = await checkRegistered(value, info);
+      if (response.status != 200) {
+        sendResponse({ message: "エラーが発生しました" });
+        return;
       }
-      sendResponse({
-        message: isNotRegistered ? "登録しました" : "既に登録されています",
-      });
+      const isRegistered = !((await response.json()).results.length === 0);
+      if (isRegistered) {
+        sendResponse({ message: "既に登録されています" });
+        return;
+      }
+
+      // 動画をNotionに登録
+      const response2 = await registerWithNotion(value, info);
+      if (response2.status == 200) {
+        sendResponse({ message: "登録しました" });
+      } else {
+        sendResponse({ message: "エラーが発生しました" });
+      }
     }
   );
   return true;
 });
 
-function registerWithNotion(value, info) {
-  const options2 = {
+async function registerWithNotion(value, info) {
+  const options = {
     method: "POST",
     headers: {
       accept: "application/json",
@@ -38,10 +49,9 @@ function registerWithNotion(value, info) {
     }),
   };
 
-  fetch("https://api.notion.com/v1/pages", options2)
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
+  return await fetch("https://api.notion.com/v1/pages", options).catch((err) =>
+    console.error(err)
+  );
 }
 
 async function checkRegistered(value, info) {
@@ -62,12 +72,8 @@ async function checkRegistered(value, info) {
       },
     }),
   };
-  const isNotRegistered = await fetch(
+  return await fetch(
     `https://api.notion.com/v1/databases/${value.databaseId}/query`,
     options
-  )
-    .then((response) => response.json())
-    .then((response) => response.results.length === 0)
-    .catch((err) => console.error(err));
-  return isNotRegistered;
+  ).catch((err) => console.error(err));
 }
